80b412a7b2fee12a305a65e75e3f17004f863f4b
B.Pardhu Chowdary <120440334+pardhu-B-chowdary@users.noreply.github.com>
2022-12-14 21:07:20 +0530

    Add files via upload

---
+++ LargestOf2.py
@@ -0,0 +1,8 @@
+a = int(input("Enter first number:"))
+b = int(input("Enter second number"))
+#or we can write a, b = map(int,input("Enter two numbers:")) 
+#to write two numbers in same line with same between the numbers same for other datatypes
+print("a=",a,"b=",b)
+if a>b:print(a)
+else:print(b)
+input()
