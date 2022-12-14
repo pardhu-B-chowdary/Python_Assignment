9b11726b9c5a4bed63a124797dad2dde6bbc89fb
B.Pardhu Chowdary <120440334+pardhu-B-chowdary@users.noreply.github.com>
2022-12-14 21:04:33 +0530

    Update First.py

--- First.py
+++ First.py
@@ -1,1 +1,3 @@
-print("Hello World")
+print("Hello World") #print String and variables
+a = 123
+pritn(f"a = {a}") #with f or format we can write variales in between string by writing the variable between { }
