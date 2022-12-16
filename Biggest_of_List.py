0ff49c9a14ad9c787ddeaf88413a871ed1d25914
B.Pardhu Chowdary <120440334+pardhu-B-chowdary@users.noreply.github.com>
2022-12-16 19:51:52 +0530

    Update Biggest_of_List.py

--- Biggest_of_List.py
+++ Biggest_of_List.py
@@ -4,7 +4,7 @@ for i in range(n):
     li[i] = int(input(f"Enter {i+1} number: "))
 #another easy  way input numbers to list with out 'n' or 'for' loop is by using map:
 #li = list(map(int,input("Enter Numbers:").split()))
-#To get max number number in List easyly --------->  print(f"Max number in the given List is : {max(li)}")
+#To get max number number in List easily --------->  print(f"Max number in the given List is : {max(li)}")
 m = 0
 for i in range(n):
     if li[i]>m:
